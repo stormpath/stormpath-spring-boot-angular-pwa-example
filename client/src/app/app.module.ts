789ba8637d5baf853374827c005b9a4ab4837616
Matt Raible <matt@raibledesigns.com>
2017-01-19 11:05:17 -0700

    Add Angular Material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { StormpathConfiguration, StormpathModule } from 'angular-stormpath';
 import { BeerListComponent } from './beer-list/beer-list.component';
+import { MaterialModule } from '@angular/material';
 
 export function stormpathConfig(): StormpathConfiguration {
   let spConfig: StormpathConfiguration = new StormpathConfiguration();
@@ -21,6 +22,7 @@ export function stormpathConfig(): StormpathConfiguration {
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule.forRoot(),
     StormpathModule
   ],
   providers: [{
